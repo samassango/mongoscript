469b1d0f4716fed8f0108b239f3a59e497808b74
samassango <samassango@gmail.com>
2020-03-30 13:13:20 +0200

    Updating mongo

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 
 var _ = require('lodash');
 var MongoClient = require('mongodb').MongoClient;
+// mongo url
 var url = "mongodb://127.0.0.1:27017/";
- console.log("***********************script start************************");
+
+ console.log("***********************script start************************");\
+
 MongoClient.connect(url, function(err, db) {
   if (err){
     console.log("Sorry unable to connect to MongoDB Error");
   }else{
     console.log("MongoDB connected...");
-    
+    // mongodb name
     var dbo = db.db("iebes_db")
+    // specify collection you targeting
     var supportingDocCollections = dbo.collection("supportingdocument");
       
     supportingDocCollections.find({}).toArray(function(err,docs){
@@ -19,6 +23,8 @@ MongoClient.connect(url, function(err, db) {
          console.log("successfully pull all documents...");
         //  console.log(docs);
         docs.forEach(doc => {
+          //Your logic to be executed for each and every documents
+
           var checkStr ="http://41.76.213.201:3000";
           var isValidLink = _.startsWith(doc.documentstr,checkStr);
            // console.log("test",isValidLink);
@@ -65,6 +71,7 @@ MongoClient.connect(url, function(err, db) {
         
         });
       }
+      // close mongo db
       db.close();
     });
 
