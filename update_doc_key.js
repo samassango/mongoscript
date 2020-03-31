a9d8ed03d52ee0d5f83753be53e88ec4d1aae753
samassango <samassango@gmail.com>
2020-03-31 08:19:52 +0200

    adding a script update and remove a specific key

---
+++ update_doc_key.js
@@ -0,0 +1,51 @@
+
+var _ = require('lodash');
+var MongoClient = require('mongodb').MongoClient;
+// mongo url
+var url = "mongodb://127.0.0.1:27017/";
+
+ console.log("***********************script start************************");
+ 
+MongoClient.connect(url, function(err, db) {
+  if (err){
+    console.log("Sorry unable to connect to MongoDB Error");
+  }else{
+    console.log("MongoDB connected...");
+    // mongodb name
+    var dbo = db.db("iebes_db")
+    // specify collection you targeting
+    var supportingDocCollections = dbo.collection("supportingdocument");
+      
+    supportingDocCollections.find({}).toArray(function(err,docs){
+      if(err){
+        console.log("Error unable to pull all documents");
+      }else{
+         console.log("successfully pull all documents...");
+        //  console.log(docs);
+        docs.forEach(doc => {
+          //Your logic to be executed for each and every documents
+
+             var documenttype = doc.docuemnttype;
+
+            supportingDocCollections.updateOne({_id:doc._id},{ $set:{documenttype},$unset:{docuemnttype:""}},function(err, res){
+            
+                if(err){
+                    console.log(err);
+                  console.log("Error updating records id-->"+doc._id)
+                }else{
+                  console.log("Success updated record id-->"+doc._id)
+                }
+              })
+          
+        
+        });
+      }
+      // close mongo db
+      db.close();
+    });
+
+  }
+
+});
+
+// console.log("***********************script end************************");
